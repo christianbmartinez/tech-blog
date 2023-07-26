887597bf0c48c2ccb10f44c480cca3f91d9dc7c5
Christian Martinez <christian@mixdlabs.com>
2023-07-26 11:49:18 -0600

    Setup /edit/:id route to edit a post, we need a view for that so i set that up as well. Its blank right now. We need to add markup in there.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -38,4 +38,47 @@ router.get('/', async (req, res) => {
     res.status(500).json(err)
   }
 })
+
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const findPost = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'post_text', 'title', 'created_on'],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'created_at',
+          ],
+          include: {
+            model: User,
+            attributes: ['full_name'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['full_name'],
+        },
+      ],
+    })
+
+    if (!findPost) {
+      res.status(404).json({ message: 'No post found' })
+      return
+    }
+
+    const post = findPost.get({ plain: true })
+
+    res.render('edit-post', { post, logged_in: req.session.logged_in })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
 module.exports = router
