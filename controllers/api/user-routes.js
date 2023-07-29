b6e601fa308dd8b9776d64b65c2715e77df3afdd
Christian Martinez <christian@mixdlabs.com>
2023-07-28 18:50:57 -0600

    Still debugging

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -36,38 +36,8 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.dataValues.id
       req.session.logged_in = true
+      res.redirect('/dashboard')
     })
-
-    const allPosts = await Post.findAll({
-      where: {
-        user_id: userData.dataValues.id,
-      },
-      attributes: ['id', 'post_text', 'title', 'created_on'],
-      include: [
-        {
-          model: Comment,
-          attributes: [
-            'id',
-            'comment_text',
-            'post_id',
-            'user_id',
-            'created_on',
-          ],
-          include: {
-            model: User,
-            attributes: ['full_name'],
-          },
-        },
-        {
-          model: User,
-          attributes: ['full_name'],
-        },
-      ],
-    })
-
-    const posts = allPosts.map((post) => post.get({ plain: true }))
-
-    res.render('dashboard', { posts, logged_in: true })
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
