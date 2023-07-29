638f507e4698f34d3a14055ed6bd05c0239d6e1d
Christian Martinez <christian@mixdlabs.com>
2023-07-28 18:33:09 -0600

    Maybe copying the route into the home routes will work, not sure. This is working fine in development

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,6 +40,48 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/', async (req, res) => {
+  if (req.session.logged_in || req.session.user_id) {
+    try {
+      const allPosts = await Post.findAll({
+        where: {
+          user_id: req.session.user_id,
+        },
+        attributes: ['id', 'post_text', 'title', 'created_on'],
+        include: [
+          {
+            model: Comment,
+            attributes: [
+              'id',
+              'comment_text',
+              'post_id',
+              'user_id',
+              'created_on',
+            ],
+            include: {
+              model: User,
+              attributes: ['full_name'],
+            },
+          },
+          {
+            model: User,
+            attributes: ['full_name'],
+          },
+        ],
+      })
+
+      const posts = allPosts.map((post) => post.get({ plain: true }))
+
+      res.render('dashboard', { posts, logged_in: true })
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+  } else {
+    res.redirect('/login')
+  }
+})
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/dashboard')
