72840476545e90114b2ac8d4288bdd0666d0eaa0
Christian Martinez <christian@mixdlabs.com>
2023-07-28 18:27:52 -0600

    Maybe forcing it to true will work

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
 
       const posts = allPosts.map((post) => post.get({ plain: true }))
 
-      res.render('dashboard', { posts, logged_in: req.session.logged_in })
+      res.render('dashboard', { posts, logged_in: true })
     } catch (err) {
       console.log(err)
       res.status(500).json(err)
@@ -79,7 +79,7 @@ router.get('/edit/:id', async (req, res) => {
 
     const post = findPost.get({ plain: true })
 
-    res.render('edit-post', { post, logged_in: req.session.logged_in })
+    res.render('edit-post', { post, logged_in: true })
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
